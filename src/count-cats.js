5da130c10fa5e7f64b5a242ba77be897b0035d61
yura <boets003@mail.ru>
2021-09-10 18:58:26 +0300

    feat: done tasks: count-cats, dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,7 +14,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+    let count = 0;
+    matrix.forEach((arr) => {
+        arr.forEach((item) => {
+            if ('^^' === item) {
+                count++
+            }
+        })
+    })
+    return count
 }
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,36 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false
+    }
+    const letters = members.map((item) => {
+        if (typeof item === 'string') {
+
+            const word = item.split('').map((l) => {
+                if (l !== ' ') {
+                    return l
+                }
+            }).join('')
+
+
+            if (word) {
+                return word[0].toUpperCase()
+            }
+        }
+    })
+    return letters.sort().join('')
 }
